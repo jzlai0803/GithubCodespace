3819d9172a0c295c70cae49713a8ad3063be8aac
GCAI0609 <70687032+jzlai0803@users.noreply.github.com>
2025-01-15 13:46:59 +0000

    123

--- ExpenseAPI/Models/Expense.cs
+++ ExpenseAPI/Models/Expense.cs
@@ -17,7 +17,7 @@ namespace ExpenseAPI.Models
         public int Id { get; set; }
         public float Amount { get; set; }
         public DateTime Date { get; set; }
-        public string Description { get; set; }
-        public string Category { get; set; }
+        public string? Description { get; set; }
+        public string? Category { get; set; }
     }
 }
